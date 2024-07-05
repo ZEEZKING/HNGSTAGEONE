e9aeda0b8e418250bd39b4c6a9a31bec750cb8a8
AbdulAzeez  Adekoya <abdulazeezadekoya17@gmail.com>
2024-07-05 09:50:55 +0100

    Add project files.

---
+++ Model/Hello.cs
@@ -0,0 +1,9 @@
+﻿namespace HNGSTAGEONE.Model
+{
+    public class Hello
+    {
+        public string? ClientIP { get; set; }
+        public string? Greetings { get; set; }
+        public string? Location { get; set; }
+    }
+}
